b68271a72097eb5e7d515f92fb3901d3cd7def87
Milosz <miloszloniewski02@gmail.com>
2023-04-25 21:45:52 +0200

    added aos lib, and styles to form states(active, error)

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,23 @@
 const projectsBoxes = document.querySelector('.projects__boxes');
+let aosPosition;
 
 fetch('data.json')
 	.then(response => response.json())
 	.then(data => {
 		for (let i = 0; i < data.length; i++) {
+			if (i % 2 == 0) {
+				aosPosition = 'right';
+			} else {
+				aosPosition = 'left';
+			}
+
 			projectsBoxes.innerHTML += `
-			<div class="projects__box">
+			<div class="projects__box" data-aos="fade-down-${aosPosition}" data-aos-delay="100">
 			<div class="projects__box-img">
-				<img src="${checkWidth(data, i)}" alt="">
-				<div class="projects__box-img-links">
-				<a href="${data[i].live}" data-text="view project" class="projects__box-img-link">view project</a>
-				<a href="${data[i].code}" data-text="view code" class="projects__box-img-link">view code</a>
+			<img src="${checkWidth(data, i)}" alt="">
+			<div class="projects__box-img-links">
+			<a href="${data[i].live}" data-text="view project" class="projects__box-img-link">view project</a>
+			<a href="${data[i].code}" data-text="view code" class="projects__box-img-link">view code</a>
 			</div>
 			</div>
 			<h3 class="projects__box-title">${data[i].title}</h3>
@@ -33,3 +40,6 @@ const checkWidth = (data, i) => {
 		return data[i].pathLarge;
 	}
 };
+
+// footer__contact-form-box--active
+// footer__contact-form-box--error
